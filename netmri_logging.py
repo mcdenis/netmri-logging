a232d73fb3a1e6ac927c4343801d9b7353c8cc28
mcdenis <maxecole@hotmail.com>
2025-04-03 11:44:12 -0400

    Fix many things.
    Ignore new log records when:
    * a log record is already being sent; or
    * the session with NetMRI has been closed.

--- netmri_logging.py
+++ netmri_logging.py
@@ -22,20 +22,81 @@ Values are case-folded.
 """
 
 
+_closed_name = f"_{__name__}__closed"
+"""
+Name of the attribute indicating that the NetMRIEasy instance has been closed.
+Follows Python's name mangling convention since this attribute is for private
+use of this module only.
+"""
+
+
+class _NetMRIClose(_deps.typing.Protocol):
+    """
+    Type of the `NetMRI.close_sessions` method.
+    """
+    
+    def __call__(_, self: _deps.NetMRIEasy) -> str: # type: ignore
+        ...
+
+
+def _netmri_close_patch(
+            close: _NetMRIClose
+        ) -> _NetMRIClose:
+    """
+    Decorator for the `NetMRIEasy.close_session` method that sets a boolean attribute on
+    the instance indicating that the method has been called. The name of the
+    attribute is defined in `_closed_name` attribute of this module.
+    """
+    
+    def decorator(
+            self: _deps.NetMRIEasy) -> str:
+        setattr(self, _closed_name, True)
+        return close(self)
+    return decorator
+
+
+def _patch_netmri(netmri: _deps.NetMRIEasy) -> None:
+    netmri_type = type(netmri)
+    if not hasattr(netmri_type, _closed_name):
+        netmri_type.close_session = _netmri_close_patch(
+            netmri_type.close_session)
+        # Set closed attribute now. We can then check whether the class has
+        # already been patched by checking the existence of the attribute.
+        setattr(netmri_type, _closed_name, False)
+
+
+_sending_count = 0
+"""
+How many records are being sent at the moment by any instance of the
+`NetMRIJobHandler` class.
+"""
+
+
 class NetMRIJobHandler(_deps.logging.Handler):
 
     def __init__(self,
                  netmri: _deps.NetMRIEasy,
                  level: _deps.typing.Union[int, str] = 0) -> None:
         super().__init__(level)
+        _patch_netmri(netmri)
         self.netmri = netmri
 
 
     def emit(self, record: _deps.logging.LogRecord) -> None:
+        global _sending_count
+        
+        # Disregard a log record if it is the result of sending a previous log
+        # record (avoid infinite recursion) or if the NetMRI instance has been
+        # exited. Not thread safe, but works with a single thread.
+        if _sending_count > 0 or getattr(self.netmri, _closed_name, False):
+            return
+
+        _sending_count+=1
+        
         level = record.levelname.casefold()
 
         # Validate logging level
-        if level not in _JOB_LOGGING_LEVELS:
+        if _deps.logging.raiseExceptions and level not in _JOB_LOGGING_LEVELS:
             raise ValueError(f"Logging level `{record.levelname}` not supported by NetMRI's Job system. Supported values are: {[l.upper() for l in _JOB_LOGGING_LEVELS]}.")
         
         # Forward log record to NetMRI. Don't use `NetMRIEasy.log_message` as it
@@ -44,11 +105,12 @@ class NetMRIJobHandler(_deps.logging.Handler):
                                                      JobDetailID=self.netmri.job_id,
                                                      severity=record.levelname.lower(),
                                                      message=self.format(record))
+        _sending_count-=1
         
 
 def create_logger(netmri: _deps.NetMRIEasy,
                   name: str,
-                  format: str = "[%(asctime)s] [%(levelname)s] %(message)s") -> _deps.logging.Logger:
+                  formatter: _deps.typing.Optional[_deps.logging.Formatter] = None) -> _deps.logging.Logger:
     """
     Create a logger for a NetMRI Job script or library.
 
@@ -58,9 +120,11 @@ def create_logger(netmri: _deps.NetMRIEasy,
     :param format: format of the string representation of the log records. As
         per `logging.Formatter`.
     """
+
+    DEFAULT_FORMAT = "[%(asctime)s] [%(levelname)s] %(message)s"
     
     handler = NetMRIJobHandler(netmri)
-    handler.setFormatter(_deps.logging.Formatter(format))
+    handler.setFormatter(formatter or _deps.logging.Formatter(DEFAULT_FORMAT))
     logger = _deps.logging.getLogger(name)
     logger.setLevel(_deps.logging.DEBUG)
     logger.addHandler(handler)
